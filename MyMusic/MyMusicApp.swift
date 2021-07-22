8c366f2b8895c8c87c48760b8e88d2bdf59bc7fb
Junpei-512 <junpei.910512.sunsys.cst@gmail.com>
2021-07-22 14:17:30 +0900

    Initial Commit

---
+++ MyMusic/MyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicApp.swift
+//  MyMusic
+//
+//  Created by Murayama Junpei on 2021/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
